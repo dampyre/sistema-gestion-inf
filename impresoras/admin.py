eee523bd1ed048f5d54f59061ce7472df2b4bdb2
dampyre <dampyre@gmail.com>
2013-04-09 17:05:15 -0300

    agregado modelo impresora y derivados 2

--- impresoras/admin.py
+++ impresoras/admin.py
@@ -1,1 +1,8 @@
-admin.py
+from impresoras.models import Impresora, marca_modelo, Conexion, Tipo_impresion, Estado
+from django.contrib import admin
+
+admin.site.register(Impresora)
+admin.site.register(marca_modelo)
+admin.site.register(Conexion)
+admin.site.register(Tipo_impresion)
+admin.site.register(Estado)
--- impresoras/models.py
+++ impresoras/models.py
@@ -2,32 +2,43 @@ from django.db import models
 import datetime
 # Create your models here.
 # Datos Base de impresora
-class Impresora(models.Model):
-	serialn = models.CharField(max_length=100, null= True)
-	marca= models.ForeignKey(MarcaModelo)
-	tipo_conexion= models.ForeignKey(Conexion)
-	tipo_impre = models.ForeignKey(Tipo_impresion)
-	estado = models.CharField(Estado)
-	fecha_ingreso = models.DateField()
+
 # ------------------------------------
 
 # marca de la impresora
-class MarcaModelo(models.Model):
-	marca = models.CharField(max_length=100)
-	modelo = models.CharField(max_length=100)	
-	
+class marca_modelo(models.Model):
+	nombre = models.CharField(max_length= 100)	
+ 	modelo= models.CharField(max_length= 100)
+ 	def __unicode__(self):
+ 		return self.nombre
 # ------------------------------------
 
 # datos de la conexion usb-wifi-red-otro
 class Conexion(models.Model):
-	tipo_conexion = models.CharField(max_length=100)
+	tipo_conexion = models.CharField(max_length= 100)
+	def __unicode__(self):
+ 		return self.tipo_conexion
 # ------------------------------------
 
 # tipo de impresion laser, inkjet etc
 class Tipo_impresion(models.Model):
-	tipo_impre = models.CharField(max_length=100)
+	tipo_impre = models.CharField(max_length= 100)
+	def __unicode__(self):
+ 		return self.tipo_impre
 # ------------------------------------
 # Estado de trabajo buena mala inactiva
 class Estado(models.Model):
-	estado = models.CharField(max_length=100)
+	estado = models.CharField(max_length= 100)
+	def __unicode__(self):
+ 		return self.estado
 
+class Impresora(models.Model):
+	serialn = models.CharField(max_length= 100, null= True)
+	nombre = models.ForeignKey(marca_modelo)
+	tipo_conexion= models.ForeignKey(Conexion)
+	tipo_impre = models.ForeignKey(Tipo_impresion)
+	estado = models.ForeignKey(Estado)
+	fecha_ingreso = models.DateField()
+	def __unicode__(self):
+		tipo= "%s %s" % (self.nombre.modelo, self.serialn)
+ 		return tipo
